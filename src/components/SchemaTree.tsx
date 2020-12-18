f5ec60483f7ebee99b2c585627657463dea4f723
yevheniiba <ev.baraniuk@gmail.com>
2020-12-18 22:53:03 +0200

    Fix array drag-n-drop jumping

--- src/components/SchemaTree.tsx
+++ src/components/SchemaTree.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import { JSONSchema7, JSONSchema7TypeName } from 'json-schema';
 import { Stack, Collapse, useDisclosure } from '@chakra-ui/react';
 import { omit } from 'lodash-es';
@@ -28,11 +28,18 @@ type SchemaTreeItemProps = Required<SchemaTreeProps> & {
 const SchemaTreeItem = (props: SchemaTreeItemProps) => {
   const { isOpen, onToggle } = useDisclosure({ defaultIsOpen: true });
   const hasSubProperties = Boolean(props.schema.type === 'object' && props.schema.properties);
+  const isArray = props.schema.type === 'array' && props.schema.items;
+
+  const getStyle = (style?: CSSProperties): CSSProperties => ({
+    ...style,
+    // this is a workaround to lock Y axis https://github.com/atlassian/react-beautiful-dnd/issues/538
+    transform: style?.transform && `translate(0px, ${style.transform.split(',').pop()}`,
+  });
 
   return (
     <Draggable draggableId={`draggable_${props.schema._metadata?.id}`} index={props.propertyIndex}>
       {(provided) => (
-        <Stack ref={provided.innerRef} {...provided.draggableProps}>
+        <Stack ref={provided.innerRef} {...provided.draggableProps} style={getStyle(provided.draggableProps.style)}>
           <SchemaProperty
             name={props.propertyName}
             required={props.required}
@@ -52,24 +59,13 @@ const SchemaTreeItem = (props: SchemaTreeItemProps) => {
               onRemove: () => props.onPropertyRemove(props.path),
             }}
           />
-          {hasSubProperties && (
+          {(hasSubProperties || isArray) && (
             <Collapse in={isOpen}>
               <SchemaTree
                 {...omit(props, 'static')} // workaround to fix making nested fields static 🙈 fix this
                 depth={props.depth + 1}
               />
             </Collapse>
-          )}
-          {props.schema.type === 'array' && props.schema.items && (
-            <SchemaTreeItem
-              {...props}
-              static
-              required
-              schema={props.schema.items as JSONSchema7}
-              path={[...props.path, 'items']}
-              depth={props.depth + 1}
-              propertyName="items"
-            />
           )}
         </Stack>
       )}
@@ -78,7 +74,7 @@ const SchemaTreeItem = (props: SchemaTreeItemProps) => {
 };
 
 export const SchemaTree = (props: SchemaTreeProps) => {
-  const { required, properties = {}, _metadata } = props.schema;
+  const { type, required, properties = {}, _metadata } = props.schema;
   const depth = props.depth ?? 0;
   const path = props.path ?? [];
 
@@ -94,25 +90,36 @@ export const SchemaTree = (props: SchemaTreeProps) => {
       <Droppable droppableId={`droppable_${_metadata?.id}`}>
         {(provided) => (
           <Stack py="1px" ref={provided.innerRef} {...provided.droppableProps}>
-            {Object.entries(properties).map(([propertyName, propertySchema], index) => {
-              if (typeof propertySchema === 'boolean') {
-                // TODO: handle this
-                return '';
-              }
+            {type === 'array' && (
+              <SchemaTreeItem
+                {...props}
+                static
+                required
+                propertyIndex={0}
+                propertyName="items"
+                key={props.schema._metadata?.id}
+                schema={props.schema.items as JSONSchema7}
+                path={[...path, 'items']}
+                depth={depth}
+              />
+            )}
+            {type === 'object' &&
+              Object.entries(properties).map(([propertyName, propertySchema], index) => {
+                if (typeof propertySchema !== 'object') return;
 
-              return (
-                <SchemaTreeItem
-                  {...props}
-                  key={propertySchema._metadata?.id}
-                  propertyIndex={index}
-                  propertyName={propertyName}
-                  required={isRequired(propertyName)}
-                  schema={propertySchema}
-                  path={[...path, 'properties', propertyName]}
-                  depth={depth}
-                />
-              );
-            })}
+                return (
+                  <SchemaTreeItem
+                    {...props}
+                    key={propertySchema._metadata?.id}
+                    propertyIndex={index}
+                    propertyName={propertyName}
+                    required={isRequired(propertyName)}
+                    schema={propertySchema}
+                    path={[...path, 'properties', propertyName]}
+                    depth={depth}
+                  />
+                );
+              })}
             {provided.placeholder}
           </Stack>
         )}
